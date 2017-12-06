f226d4150687bf7a3c1961189a2bb9ca115e1113
xueyao1220 <xueyao1991@gmail.com>
2017-12-06 11:19:22 +0100

    Create project file for Yao_Reader

---
+++ Yao_Reader.c
@@ -0,0 +1,70 @@
+//==============================================================================
+//
+// Title:		Yao_Reader
+// Purpose:		A short description of the application.
+//
+// Created on:	06.12.2017 at 11:18:36 by Yao Xue.
+// Copyright:	Christoph Miethke GmbH & Co. KG. All Rights Reserved.
+//
+//==============================================================================
+
+//==============================================================================
+// Include files
+
+#include <ansi_c.h>
+#include <cvirte.h>		
+#include <userint.h>
+#include "Yao_Reader.h"
+#include "toolbox.h"
+
+//==============================================================================
+// Constants
+
+//==============================================================================
+// Types
+
+//==============================================================================
+// Static global variables
+
+static int panelHandle = 0;
+
+//==============================================================================
+// Static functions
+
+//==============================================================================
+// Global variables
+
+//==============================================================================
+// Global functions
+
+/// HIFN The main entry-point function.
+int main (int argc, char *argv[])
+{
+	int error = 0;
+	
+	/* initialize and load resources */
+	nullChk (InitCVIRTE (0, argv, 0));
+	errChk (panelHandle = LoadPanel (0, "Yao_Reader.uir", PANEL));
+	
+	/* display the panel and run the user interface */
+	errChk (DisplayPanel (panelHandle));
+	errChk (RunUserInterface ());
+
+Error:
+	/* clean up */
+	if (panelHandle > 0)
+		DiscardPanel (panelHandle);
+	return 0;
+}
+
+//==============================================================================
+// UI callback function prototypes
+
+/// HIFN Exit when the user dismisses the panel.
+int CVICALLBACK panelCB (int panel, int event, void *callbackData,
+		int eventData1, int eventData2)
+{
+	if (event == EVENT_CLOSE)
+		QuitUserInterface (0);
+	return 0;
+}
---
+++ Yao_Reader.h
@@ -0,0 +1,36 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  PANEL                            1       /* callback function: panelCB */
+
+
+     /* Control Arrays: */
+
+          /* (no control arrays in the resource file) */
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* Callback Prototypes: */
+
+int  CVICALLBACK panelCB(int panel, int event, void *callbackData, int eventData1, int eventData2);
+
+
+#ifdef __cplusplus
+    }
+#endif
