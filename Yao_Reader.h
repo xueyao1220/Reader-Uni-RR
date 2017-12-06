608c0d0d96a25895b79e985017439bb1e1cb1c00
xueyao1220 <xueyao1991@gmail.com>
2017-12-06 12:21:50 +0100

    UI Commit: First Edition of UI

--- Yao_Reader.h
+++ Yao_Reader.h
@@ -14,6 +14,16 @@
      /* Panels and Controls: */
 
 #define  PANEL                            1       /* callback function: panelCB */
+#define  PANEL_QUITBUTTON                 2       /* control type: command, callback function: QuitCallback */
+#define  PANEL_DECORATION                 3       /* control type: deco, callback function: (none) */
+#define  PANEL_iLBComPortList             4       /* control type: listBox, callback function: (none) */
+#define  PANEL_iBcomPortClose             5       /* control type: command, callback function: OnCloseComPortPressed */
+#define  PANEL_iBComPortOpen              6       /* control type: command, callback function: OnOpenComPortPressed */
+#define  PANEL_sTBComPortStatus           7       /* control type: textBox, callback function: (none) */
+#define  PANEL_DECORATION_2               8       /* control type: deco, callback function: (none) */
+#define  PANEL_sSerialNr                  9       /* control type: string, callback function: (none) */
+#define  PANEL_sAsicID                    10      /* control type: string, callback function: (none) */
+#define  PANEL_COMMANDBUTTON              11      /* control type: command, callback function: (none) */
 
 
      /* Control Arrays: */
@@ -28,7 +38,10 @@
 
      /* Callback Prototypes: */
 
+int  CVICALLBACK OnCloseComPortPressed(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK OnOpenComPortPressed(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK panelCB(int panel, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK QuitCallback(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
